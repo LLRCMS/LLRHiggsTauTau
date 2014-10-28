d2b81278da7939c3184b35861fe110f3d43f9385
Giacomo Ortona <ortona@llrui01.in2p3.fr>
2014-10-28 12:22:59 +0100

    readme, mufiller and elefiller

---
+++ README.md
@@ -0,0 +1,27 @@
+This package provides tools to READ miniAOD events and store the results in an TNtuple optimized for the HTauTau analysis.
+
+The package workflow is the following:
+Main flags/cut/configuration tools can be set in analyzer.py
+Modules and plugins are called by python/HiggsTauTauProducer.py 
+	Cuts on leptons/pairs are also set here
+	This module creates AF/OS and AF/SS pairs, runs the SVfit on the pairs and store the useful variables in a TNtuple
+The stored variables are set in plugins/HTauTauNtupleMaker.cc
+
+Instruction to get and run the package:
+git clone https://github.com/LLRCMS/LLRHiggsTauTau.py 
+(cd LLRHiggsTauTau; git checkout master)
+
+#effective areas (to be updated)
+git clone -n https://github.com/latinos/UserCode-sixie-Muon-MuonAnalysisTools Muon/MuonAnalysisTools
+(cd Muon/MuonAnalysisTools ; git checkout master -- interface/MuonEffectiveArea.h )
+
+#Note we did not eventually adopt the latest EA update, we kept V00-00-30-00 of UserCode/EGamma/EGammaAnalysisTools/interface/ElectronEffectiveArea.h .  c0db796 corresponds to it.
+git clone -n https://github.com/cms-analysis/EgammaAnalysis-ElectronTools EGamma/EGammaAnalysisTools
+(cd EGamma/EGammaAnalysisTools; git checkout c0db796 -- interface/ElectronEffectiveArea.h) 
+
+#MuScleFit: probably tbf
+#git clone https://github.com/scasasso/usercode MuScleFit
+
+Quick usage:
+Define the files you want to run in analyzer.py and run cmsRun analyzer.py
+
--- python/HiggsTauTauProducer.py
+++ python/HiggsTauTauProducer.py
@@ -163,6 +163,129 @@ else:
     process.muons =  cms.Sequence(process.cleanedMu + process.bareSoftMuons + process.softMuons)
     
 
+##
+## Electrons
+##
+#--- Electron regression+calibrarion must be applied after BDT is recomputed
+# NOTE patElectronsWithRegression->eleRegressionEnergy;  calibratedElectrons-> calibratedPatElectrons
+# Default: NEW ECAL regression + NEW calibration + NEW combination
+process.load('EgammaAnalysis.ElectronTools.electronRegressionEnergyProducer_cfi')
+process.eleRegressionEnergy.inputElectronsTag = cms.InputTag('patElectronsWithTrigger')
+process.eleRegressionEnergy.energyRegressionType = 2 ## 1: ECAL regression w/o subclusters 2 (default): ECAL regression w/ subclusters)
+#process.eleRegressionEnergy.vertexCollection = cms.InputTag('goodPrimaryVertices')
+process.load("EgammaAnalysis.ElectronTools.calibratedPatElectrons_cfi")
+process.calibratedPatElectrons.correctionsType = 2 # 1 = old regression, 2 = new regression, 3 = no regression, 0 = nothing
+process.calibratedPatElectrons.combinationType = 3
+process.calibratedPatElectrons.lumiRatio = cms.double(1.0)
+process.calibratedPatElectrons.isMC    = IsMC
+process.calibratedPatElectrons.synchronization = cms.bool(False)
+
+if (LEPTON_SETUP == 2011):
+   process.eleRegressionEnergy.rhoCollection = cms.InputTag('kt6PFJetsForIso:rho')
+   if (IsMC):
+       process.calibratedPatElectrons.inputDataset = "Fall11"
+   else :
+       process.calibratedPatElectrons.inputDataset = "Jan16ReReco"
+else :
+   if (IsMC):
+       process.calibratedPatElectrons.inputDataset = "Summer12_LegacyPaper"
+   else :
+       process.calibratedPatElectrons.inputDataset = "22Jan2013ReReco"
+
+    
+
+process.bareSoftElectrons = cms.EDFilter("PATElectronRefSelector",
+   src = cms.InputTag("calibratedPatElectrons"),
+   cut = cms.string("pt>7 && abs(eta)<2.5 &&" +
+                    "gsfTrack.trackerExpectedHitsInner.numberOfHits<=1"
+                    )
+   )
+
+process.softElectrons = cms.EDProducer("EleFiller",
+   src    = cms.InputTag("bareSoftElectrons"),
+   sampleType = cms.int32(SAMPLE_TYPE),          
+   setup = cms.int32(LEPTON_SETUP), # define the set of effective areas, rho corrections, etc.
+#    cut = cms.string("userFloat('SIP')<100"),
+   cut = cms.string("userFloat('dxy')<0.5 && userFloat('dz')<1"),
+   flags = cms.PSet(
+        ID = cms.string("userFloat('isBDT')"), # BDT MVA ID
+        isGood = cms.string(GOODLEPTON)
+        )
+   )
+
+
+process.electrons = cms.Sequence(process.eleRegressionEnergy + process.calibratedPatElectrons + process.bareSoftElectrons + process.softElectrons)
+
+# Handle special cases
+if ELEREGRESSION == "None" and ELECORRTYPE == "None" :   # No correction at all. Skip correction modules.
+    process.bareSoftElectrons.src = cms.InputTag('slimmedElectrons')#patElectronsWithTrigger')#RH
+    process.electrons = cms.Sequence(process.bareSoftElectrons + process.softElectrons)
+
+elif ELEREGRESSION == "Moriond" and ELECORRTYPE == "Moriond" : # Moriond corrections: OLD ECAL regression + OLD calibration + OLD combination 
+    if (LEPTON_SETUP == 2011):
+        process.eleRegressionEnergy.regressionInputFile = cms.string("EgammaAnalysis/ElectronTools/data/eleEnergyReg2011Weights_V1.root")
+    else :
+        process.eleRegressionEnergy.regressionInputFile = cms.string("EgammaAnalysis/ElectronTools/data/eleEnergyReg2012Weights_V1.root")
+    process.eleRegressionEnergy.energyRegressionType = 1
+    process.calibratedPatElectrons.correctionsType   = 1
+    process.calibratedPatElectrons.combinationType   = 1
+
+elif ELEREGRESSION == "Paper" and ELECORRTYPE == "None" : # NEW ECAL regression + NO calibration + NO combination
+    process.eleRegressionEnergy.energyRegressionType = 2
+    process.calibratedPatElectrons.correctionsType   = 0
+    process.calibratedPatElectrons.combinationType   = 0
+
+elif ELEREGRESSION == "Paper" and ELECORRTYPE == "PaperNoComb" : # NEW ECAL regression + NEW calibration + NO combination
+    process.eleRegressionEnergy.energyRegressionType = 2
+    process.calibratedPatElectrons.correctionsType   = 1
+    process.calibratedPatElectrons.combinationType   = 0
+    
+
+process.electronMatch = cms.EDProducer("MCMatcher",       # cut on deltaR, deltaPt/Pt; pick best by deltaR
+                                       src         = cms.InputTag("bareSoftElectrons"), # RECO objects to match
+                                       matched     = cms.InputTag("genParticlesPruned"), # mc-truth particle collection
+                                       mcPdgId     = cms.vint32(11),               # one or more PDG ID (11 = electron); absolute values (see below)
+                                       checkCharge = cms.bool(True),               # True = require RECO and MC objects to have the same charge
+                                       mcStatus    = cms.vint32(1),                # PYTHIA status code (1 = stable, 2 = shower, 3 = hard scattering)
+                                       maxDeltaR   = cms.double(0.5),              # Minimum deltaR for the match
+                                       maxDPtRel   = cms.double(0.5),              # Minimum deltaPt/Pt for the match
+                                       resolveAmbiguities    = cms.bool(True),     # Forbid two RECO objects to match to the same GEN object
+                                       resolveByMatchQuality = cms.bool(False),    # False = just match input in order; True = pick lowest deltaR pair first
+                                       )
+
+
+### ----------------------------------------------------------------------
+### Lepton Cleaning (clean electrons collection from muons)
+### ----------------------------------------------------------------------
+
+process.cleanSoftElectrons = cms.EDProducer("PATElectronCleaner",
+    # pat electron input source
+    src = cms.InputTag("softElectrons"),
+    # preselection (any string-based cut for pat::Electron)
+    preselection = cms.string(''),
+    # overlap checking configurables
+    checkOverlaps = cms.PSet(
+        muons = cms.PSet(
+           src       = cms.InputTag("softMuons"), # Start from loose lepton def
+           algorithm = cms.string("byDeltaR"),
+           preselection        = cms.string("(isGlobalMuon || userFloat('isPFMuon'))"), #
+           deltaR              = cms.double(0.05),  
+           checkRecoComponents = cms.bool(False), # don't check if they share some AOD object ref
+           pairCut             = cms.string(""),
+           requireNoOverlaps   = cms.bool(True), # overlaps don't cause the electron to be discared
+        )
+    ),
+    # finalCut (any string-based cut for pat::Electron)
+    finalCut = cms.string(''),
+)
+
+##
+## Taus
+##
+process.taus = cms.EDFilter("PATTauRefSelector",
+   src = cms.InputTag("slimmedTaus"),
+   cut = cms.string("pt>7 && abs(eta)<2.5 &&")
+   )
 
 #Leptons
 process.softLeptons = cms.EDProducer("CandViewMerger",
@@ -185,3 +308,30 @@ process.barellCand = cms.EDProducer("CandViewShallowCombiner",
 ##
 ## SV fit
 ##
+process.SVllCand = cms.EDProducer("SVfitter",
+                                  src = cms.VInputTag("barellCand"))
+
+##
+## Paths
+##
+
+process.PVfilter = cms.Path(process.goodPrimaryVertices)
+
+# Prepare lepton collections
+process.Candidates = cms.Path(
+       process.muons             +
+       process.electrons         +  #process.cleanSoftElectrons +
+       #process.fsrPhotonSequence + process.appendPhotons     +#RH
+       process.taus              +
+       process.softLeptons       +
+# Build dilepton candidates
+       process.SVllCand
+       #process.bareEECand        + process.EECand   +  
+       #process.bareMMCand        + process.MMCand   + 
+       #process.bareMMMMCand      + process.MMMMCand  +
+       #process.bareEEEECand      + process.EEEECand  +
+       #process.bareEEMMCand      + process.EEMMCand  +
+       #process.ZZCand
+  )
+
+SkimPaths = cms.vstring('PVfilter') #Do not apply skim 
