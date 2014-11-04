0878dcc31e010ce8e5bc5682c7cd800305ea32c7
Giacomo Ortona <gortona@cern.ch>
2014-11-04 18:22:32 +0100

    everything compiles

--- NtupleProducer/plugins/HTauTauNtuplizer.cc
+++ NtupleProducer/plugins/HTauTauNtuplizer.cc
@@ -12,6 +12,7 @@
 #include <cmath>
 #include <vector>
 #include <string>
+#include <TNtuple.h>
 
 // user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
@@ -51,19 +52,19 @@
 //#include <LLRHiggsTauTau/NtupleProducer/interface/DaughterDataHelpers.h>
 //#include <LLRHiggsTauTau/NtupleProducer/interface/FinalStates.h>
 #include <LLRHiggsTauTau/NtupleProducer/interface/MCHistoryTools.h>
-//#include <LLRHiggsTauTau/NtupleProducer/interface/PUReweight.h>
+#include <LLRHiggsTauTau/NtupleProducer/interface/PUReweight.h>
 //#include <LLRHiggsTauTau/NtupleProducer/interface/VBFCandidateJetSelector.h>
 //#include <LLRHiggsTauTau/NtupleProducer/interface/bitops.h>
 //#include <LLRHiggsTauTau/NtupleProducer/interface/Fisher.h>
-#include <LLRHiggsTauTau/NtupleProducer/interface/HTauTauConfigHelper.h>
+//#include <LLRHiggsTauTau/NtupleProducer/interface/HTauTauConfigHelper.h>
 //#include "HZZ4lNtupleFactory.h"
 
 #include "TLorentzVector.h"
 
-namespace {
-  bool writePhotons = false;  // Write photons in the tree. 
-  bool writeJets = true;     // Write jets in the tree. 
-}
+// namespace {
+//   bool writePhotons = false;  // Write photons in the tree. 
+//   bool writeJets = true;     // Write jets in the tree. 
+// }
 
 
 using namespace std;
@@ -78,8 +79,9 @@ class HTauTauNtuplizer : public edm::EDAnalyzer {
   explicit HTauTauNtuplizer(const edm::ParameterSet&);
     
   /// Destructor
-  ~HTauTauNtuplizer(){};  
+  ~HTauTauNtuplizer();  
 
+  TString makeVarString();
  private:
   //----edm control---
   virtual void beginJob() ;
@@ -89,14 +91,13 @@ class HTauTauNtuplizer : public edm::EDAnalyzer {
   virtual void endRun(edm::Run const&, edm::EventSetup const&);
   virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
   virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
-
   //----To implement here-----
- virtual void FillCandidate(const pat::CompositeCandidate& higgs, bool evtPass, const edm::Event&, const Int_t CRflag);
-  virtual void FillPhoton(const cmg::Photon& photon);
-  virtual void FillJet(const cmg::PFJet& jet);
+  //virtual void FillCandidate(const pat::CompositeCandidate& higgs, bool evtPass, const edm::Event&, const Int_t CRflag);
+  //virtual void FillPhoton(const pat::Photon& photon);
+  //virtual void FillJet(const cmg::PFJet& jet);
 
   // ----------member data ---------------------------
-  HTauTauConfigHelper myHelper;
+  //HTauTauConfigHelper myHelper;
   int theChannel;
   std::string theCandLabel;
   TString theFileName;
@@ -104,21 +105,66 @@ class HTauTauNtuplizer : public edm::EDAnalyzer {
   TNtuple *myTree;//->See from ntuplefactory in zz4l
   TH1F *hCounter;
 
+  static const int nOutVars =14;
+
   Bool_t isMC;
 
   bool applyTrigger;    // Only events passing trigger
   bool applySkim;       //  "     "      "     skim
   bool skipEmptyEvents; // Skip events whith no candidate in the collection
 
-  PUReweight reweight;
+  //PUReweight reweight;
 
   //counters and branches
-  Float_t Nevt_Gen;
+  Int_t Nevt_Gen;
+  Int_t Npairs;
 
 };
 
 // ----Constructor and Desctructor -----
+HTauTauNtuplizer::HTauTauNtuplizer(const edm::ParameterSet& pset) {
+  theCandLabel = pset.getUntrackedParameter<string>("CandCollection");
+  //theChannel = myHelper.channel();
+  theFileName = pset.getUntrackedParameter<string>("fileName");
+  skipEmptyEvents = pset.getParameter<bool>("skipEmptyEvents");
+  //writeBestCandOnly = pset.getParameter<bool>("onlyBestCandidate");
+  //sampleName = pset.getParameter<string>("sampleName");
+  Nevt_Gen=0;
+  Npairs=0;
+}
 
+HTauTauNtuplizer::~HTauTauNtuplizer(){}
+//
+void HTauTauNtuplizer::beginJob(){
+  edm::Service<TFileService> fs;
+  TString varString = makeVarString();
+  myTree = fs->make<TNtuple>("HTauTauNtuple","HTauTauNtuple",varString.Data());
+  hCounter = fs->make<TH1F>("Counters","Counters",2,0,2);
+}
+
+//For semplicity and flexibility, list output variables here. Possibly, amke flags for different
+//outputs, but take care of modifying the filling acocrdingly
+TString HTauTauNtuplizer::makeVarString(){
+  TString varlist[nOutVars] = {
+    "run",
+    "iev",
+    "P0Pair",
+    "P1Pair",
+    "P2Pair",
+    "P3Pair",
+    "P01",
+    "P11",
+    "P21",
+    "P31",
+    "P02",
+    "P12",
+    "P22",
+    "P32"
+  };
+  TString out = varlist[0].Data();
+  for(int i=1;i<nOutVars;i++){out.Append(":");out.Append(varlist[i].Data());}
+  return out;
+}
 // ----Analyzer (main) ----
 
 // ------------ method called for each event  ------------
@@ -130,26 +176,89 @@ void HTauTauNtuplizer::analyze(const edm::Event& event, const edm::EventSetup& e
   //----------------------------------------------------------------------
   // Analyze MC history. THIS HAS TO BE DONE BEFORE ANY RETURN STATEMENT
   // (eg skim or trigger), in order to update the gen counters correctly!!!
-  int nObsInt  = -1;
-  float nTrueInt = -1.;
-  Float_t weight2 = 1.;
-  Int_t genFinalState = -1;
-  Int_t genProcessId = -1;
-  Float_t genHEPMCweight = 1.;
-  Short_t genExtInfo = -1;
-};
+  
+  //  std::vector<const reco::Candidate *> genZs;
+  // std::vector<const reco::Candidate *> genZLeps;
+  // if (isMC) {
+    
+  //   Handle<std::vector< PileupSummaryInfo > >  PupInfo;
+  //   event.getByLabel(edm::InputTag("addPileupInfo"), PupInfo);
+    
+  //   std::vector<PileupSummaryInfo>::const_iterator PVI;
+  //   for(PVI = PupInfo->begin(); PVI != PupInfo->end(); ++PVI) {
+  //     if(PVI->getBunchCrossing() == 0) { 
+  // 	nObsInt  = PVI->getPU_NumInteractions();
+  // 	nTrueInt = PVI->getTrueNumInteractions();
+  // 	break;
+  //     } 
+  //   }
+  // }
 
-// ----NtupleFiller ----
 
+  //Get candidate collection
+  edm::Handle<edm::View<pat::CompositeCandidate>>candHandle;
+  event.getByLabel("barellCand",candHandle);
+  const edm::View<pat::CompositeCandidate>* cands = candHandle.product();
 
-// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
-void HTauTauNtuplizer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
-  //The following says we do not know what parameters are allowed so do no validation
-  // Please change this to state exactly what you do use, even if it is no parameters
-  edm::ParameterSetDescription desc;
-  desc.setUnknown();
-  descriptions.addDefault(desc);
+  //myNtuple->InitializeVariables();
+
+  for(edm::View<pat::CompositeCandidate>::const_iterator candi = cands->begin(); candi!=cands->end();++candi){
+    Npairs++;
+    const pat::CompositeCandidate& cand = (*candi);
+    //Do all the stuff here
+    //Compute the variables needed for the output and store them in the ntuple
+    float fillArray[nOutVars]={
+      (float)event.id().run(),
+      (float)event.id().event(),
+      (float)cand.p4().mass(),
+      (float)cand.p4().pt(),
+      (float)cand.p4().eta(),
+      (float)cand.p4().phi(),
+      (float)cand.daughter(0)->mass(),
+      (float)cand.daughter(0)->pt(),
+      (float)cand.daughter(0)->eta(),
+      (float)cand.daughter(0)->phi(),
+      (float)cand.daughter(1)->mass(),
+      (float)cand.daughter(1)->pt(),
+      (float)cand.daughter(1)->eta(),
+      (float)cand.daughter(1)->phi()
+    };
+    myTree->Fill(fillArray);
+  }
+
 }
+
+void HTauTauNtuplizer::endJob(){
+  hCounter->SetBinContent(0,Nevt_Gen);
+  hCounter->SetBinContent(1,Npairs);
+}
+
+void HTauTauNtuplizer::beginRun(edm::Run const&, edm::EventSetup const&){
+}
+void HTauTauNtuplizer::endRun(edm::Run const&, edm::EventSetup const&){
+}
+void HTauTauNtuplizer::beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&){
+}
+void HTauTauNtuplizer::endLuminosityBlock(edm::LuminosityBlock const& iLumi, edm::EventSetup const& iSetup)
+{
+  Float_t Nevt_preskim = -1.;
+  edm::Handle<edm::MergeableCounter> preSkimCounter;
+  if (iLumi.getByLabel("preSkimCounter", preSkimCounter)) { // Counter before skim. Does not exist for non-skimmed samples.
+    Nevt_preskim = preSkimCounter->value;
+  }  
+  if (Nevt_preskim>=0.) {
+    Nevt_Gen = Nevt_Gen + Nevt_preskim; 
+  } 
+}
+
+// // ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
+// void HTauTauNtuplizer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+//   //The following says we do not know what parameters are allowed so do no validation
+//   // Please change this to state exactly what you do use, even if it is no parameters
+//   edm::ParameterSetDescription desc;
+//   desc.setUnknown();
+//   descriptions.addDefault(desc);
+// }
 
 //define this as a plug-in
 DEFINE_FWK_MODULE(HTauTauNtuplizer);
