e011b512ec45d0c5027a32f1b8e1899f26a3d2bb
Luca Cadamuro <lc.cadamuro@gmail.com>
2014-11-18 18:18:38 +0100

    Still need MEt interface for SVfit

---
+++ NtupleProducer/plugins/SVfitInterface.cc
@@ -0,0 +1,11 @@
+/* 
+** This class computes the SVfit mass of the lepton pair candidates
+** 
+** Input pairs contain a flag describing their decay mode (e, mu, tauh)
+** to be used in the algorithm.
+**
+** input is reco::CompositeCandidate for each lepton
+** that is coverted to TLorentzVector to be passed to the algorithm 
+*/
+
+#include "TauAnalysis/SVfitStandalone/interface/SVfitStandaloneAlgorithm.h"
