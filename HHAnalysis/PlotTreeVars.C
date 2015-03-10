fa08e980218a1aac164e50b1e9977e888e341b03
Giacomo Ortona <gortona@cern.ch>
2015-03-10 13:18:38 +0100

    update

--- HHAnalysis/PlotTreeVars.C
+++ HHAnalysis/PlotTreeVars.C
@@ -2,21 +2,27 @@
 #include <iostream>
 #include "TTree.h"
 #include "TFile.h"
-#include "TH1D.h"
+#include "TH1F.h"
 #include "TCanvas.h"
+#include "TLorentzVector.h"
+
+//For trigger selction tools, please refer to 
+//https://github.com/LLRCMS/LLRHiggsTauTau/blob/master/NtupleProducer/Utils/OfflineProducerHelper.h
 
 using namespace std;
 
 void PlotTreeVars()
 {
     TFile* fIn = new TFile ("/home/llr/cms/cadamuro/HiggsTauTauFramework/CMSSW_7_2_0/src/LLRHiggsTauTau/NtupleProducer/test/HTauTauAnalysis.root"); 
-    TTree* treePtr = (TTree*) fIn -> Get ("HTauTauTree/HTauTauTree");
     
+    TTree* treePtr = (TTree*) fIn->Get("HTauTauTree/HTauTauTree");
+    TH1F *evCounter = (TH1F*) fIn->Get("HTauTauTree/Counters");
+    
+    cout<<"Number of Events analysed "<<evCounter->GetBinContent(1)<<endl;
+    cout<<"Number of Events Passing at least one trigger "<<evCounter->GetBinContent(2)<<endl;
+    
+    //HTauTauTree class defines the branches from the Ntuplizer output
     HTauTauTree* tree = new HTauTauTree (treePtr);
-    
-    // histograms
-    TH1D* bDiscr = new TH1D ("h_bDiscr", "bDiscriminator", 20, 0, 10);
-    TH1D* h_mothers_px = new TH1D ("h_mothers_px", "mothers_px", 100, -100, 100);
     
     /*
     // Can activate only some branches like this
@@ -25,20 +31,47 @@ void PlotTreeVars()
     tree->GetTree()->SetBranchStatus("mothers_px", 1);
     */
 
+    // histograms
+    TH1F* bDiscr = new TH1F ("h_bDiscr", "bDiscriminator", 20, 0, 10);
+    TH1F* h_mothers_px = new TH1F ("h_mothers_px", "mothers_px", 100, -100, 100);
+    TH1F* h_mothers_SVmass = new TH1F ("h_mothers_SVfitMass", "mothers_SVfitMass", 100, -100, 800);
+    TH2F *h_dauPtvsSVFit = new TH2F("dauPtvsSVMass","dauPtvsSVMass",100,-100,800,100,0,1000);
     cout << "TreeEntries: " << tree->GetEntries() << endl;
     for (int iEntry = 0; iEntry < tree->GetEntries() ; iEntry++)
     {
         tree->GetEntry(iEntry);
         
+        //Distribution of the bDiscriminant
         for (int iJet = 0; iJet < tree->bDiscriminator->size(); iJet++)
         {
             bDiscr->Fill (tree->bDiscriminator->at(iJet));
         }
         
+        //Distributions for the pairs and for the daughters
         for (int iMoth = 0; iMoth < tree->mothers_px->size(); iMoth++)
         {
             h_mothers_px->Fill(tree->mothers_px->at(iMoth));
+            float svmass=tree->SVfitMass->at(iMoth);
+            h_mothers_SVmass->Fill(svmass);
+            //Distribution of daughters pt as a function of the pair invariant mass
+            //indexes of the daughters (in the daughters vector)
+            dau1index = tree->indexDau1->at(iMoth);
+            dau2index = tree->indexDau2->at(iMoth);
+            //daughters four momenta
+            dau1px = tree->daughters_px->at(dau1index);
+            dau1py = tree->daughters_py->at(dau1index);
+            dau1pz = tree->daughters_pz->at(dau1index);
+            dau1e = tree->daughters_e->at(dau1index);
+            dau2px = tree->daughters_px->at(dau2index);
+            dau2py = tree->daughters_py->at(dau2index);
+            dau2pz = tree->daughters_pz->at(dau2index);
+            dau2e = tree->daughters_e->at(dau2index);
+            TLorentzVector dau1(dau1px, dau1py, dau1pz, dau1e);
+            TLorentzVector dau2(dau2px, dau2py, dau2pz, dau2e);
+            h_dauPtvsSVFit->Fill(svmass, dau1.Pt());
+            h_dauPtvsSVFit->Fill(svmass, dau2.Pt());
         }
+        
     }
     
     TCanvas * c1 = new TCanvas;
@@ -46,6 +79,12 @@ void PlotTreeVars()
     
     TCanvas * c2 = new TCanvas;
     h_mothers_px->Draw();
+    
+    TCanvas * c3 = new TCanvas();
+    h_dauPtvsSVFit->Draw("COLZ");
+    
+    TCanvas *c4 = new TCanvas();
+    h_mothers_SVmass->Draw();
     
     //c2->Print ("mothers_px.png", "png");
     
