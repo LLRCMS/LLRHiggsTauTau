f67176fe6a679aa3d7bf20aad2255c2d583d50ae
Roberto Salerno <roberto.salerno@cern.ch>
2015-07-19 15:44:22 +0200

    Update with the last Golden JSON file

--- NtupleProducer/test/tools/ConvertJSON.py
+++ NtupleProducer/test/tools/ConvertJSON.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-json = eval( open('/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/DCSOnly/json_DCSONLY_Run2015B.txt').read() ) #for Run2015B DCS only JSON
+json = eval( open('/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Cert_246908-251252_13TeV_PromptReco_Collisions15_JSON.txt').read() ) #for Run2015B DCS only JSON
 
 print 'process.source.lumisToProcess = cms.untracked.VLuminosityBlockRange( *('
 for run,lumisections in sorted(json.iteritems()):
