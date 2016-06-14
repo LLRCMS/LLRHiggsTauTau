e9c21da561862b8f61f7cb980f774d8e23d9e936
Luca Cadamuro <lc.cadamuro@gmail.com>
2016-06-14 13:26:41 +0200

    oops

--- NtupleProducer/plugins/HTauTauNtuplizer.cc
+++ NtupleProducer/plugins/HTauTauNtuplizer.cc
@@ -2226,9 +2226,9 @@ void HTauTauNtuplizer::FillSoftLeptons(const edm::View<reco::Candidate> *daus, c
             //_isFilterFiredLast;
             if(isfilterGood)filterFired |= 1 <<triggerbit;
             if (triggerType) triggertypeIsGood |= 1 << triggerbit;
+            if(isLF)LFtriggerbit |= 1 <<triggerbit;
+            if(isL3)L3triggerbit |= 1 <<triggerbit;
           }
-          if(isLF)LFtriggerbit |= 1 <<triggerbit;
-          if(isL3)L3triggerbit |= 1 <<triggerbit;
         } // loop on all trigger paths
       } // if dR < 0.25
     } // loop on all trigger candidates
