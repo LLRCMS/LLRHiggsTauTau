a41d3b21a7f9a43ee81f3df644d7dc0d45cbf108
Michal Bluj <michal.bluj@cern.ch>
2016-06-28 11:26:33 +0200

    Header protection added

--- NtupleProducer/interface/PhotonFwd.h
+++ NtupleProducer/interface/PhotonFwd.h
@@ -1,3 +1,6 @@
+#ifndef LLRHiggsTauTau_NtupleProducer_PhotonFwd_h
+#define LLRHiggsTauTau_NtupleProducer_PhotonFwd_h
+
 #include <DataFormats/PatCandidates/interface/Photon.h>
 #include <DataFormats/PatCandidates/interface/PFParticle.h>
 #include <vector>
@@ -6,3 +9,4 @@ typedef pat::PFParticle        Photon;
 typedef edm::Ptr<Photon>       PhotonPtr;
 typedef std::vector<PhotonPtr> PhotonPtrVector;
 
+#endif
