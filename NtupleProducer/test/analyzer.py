755184f8c7fa74f1ee0aa6f5cc702abc7d623a38
Artur Kalinowski <Artur.Kalinowski@cern.ch>
2016-10-10 14:19:13 +0200

    * put back protection agains pathological tau candidates, but with relaxed criteria

--- NtupleProducer/test/analyzer.py
+++ NtupleProducer/test/analyzer.py
@@ -40,7 +40,7 @@ TAUDISCRIMINATOR="byIsolationMVA3oldDMwoLTraw"
 PVERTEXCUT="!isFake && ndof > 4 && abs(z) <= 24 && position.Rho <= 2" #cut on good primary vertexes
 MUCUT="isLooseMuon && pt>5"
 ELECUT="pt>7"#"gsfTrack.hitPattern().numberOfHits(HitPattern::MISSING_INNER_HITS)<=1 && pt>10"
-TAUCUT="pt>18" #miniAOD tau from hpsPFTauProducer have pt>18 and decaymodefinding ID
+TAUCUT="(tauID('byCombinedIsolationDeltaBetaCorrRaw3Hits') < 1000.0 || tauID('byIsolationMVArun2v1DBoldDMwLTraw')>-0.999) && pt>18" #miniAOD tau from hpsPFTauProducer have pt>18 and decaymodefinding ID
 JETCUT="pt>10"
 LLCUT="mass>0"
 BCUT="pt>5"
