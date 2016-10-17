61dceeccb1ff8f94da06dff8457aa58253eeea28
giacomoortona <gortona@cern.ch>
2016-10-17 14:54:09 +0200

    protection for duplicated mu

--- NtupleProducer/test/analyzer_treeFromEnrMiniAOD.py
+++ NtupleProducer/test/analyzer_treeFromEnrMiniAOD.py
@@ -41,7 +41,7 @@ MUCUT="isLooseMuon && pt>5"
 ELECUT="pt>7"#"gsfTrack.hitPattern().numberOfHits(HitPattern::MISSING_INNER_HITS)<=1 && pt>10"
 TAUCUT="tauID('byCombinedIsolationDeltaBetaCorrRaw3Hits') < 1000.0 && pt>18" #miniAOD tau from hpsPFTauProducer have pt>18 and decaymodefinding ID
 JETCUT="pt>10"
-LLCUT="mass>0"
+LLCUT="mass>0 && ditau_deltaR>0"
 BCUT="pt>5"
 
 # ------------------------
