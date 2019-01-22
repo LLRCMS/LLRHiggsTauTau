9be4332033f4b662d906c120308b96adf5c5b56c
Francesco Brivio <francesco.brivio@cern.ch>
2019-01-22 10:02:35 +0100

    fix bug in METCorrections module

--- NtupleProducer/python/HiggsTauTauProducer_94X.py
+++ NtupleProducer/python/HiggsTauTauProducer_94X.py
@@ -585,7 +585,7 @@ from PhysicsTools.PatUtils.tools.runMETCorrectionsAndUncertainties import runMet
 
 runMetCorAndUncFromMiniAOD (
         process,
-        isData = True, # false for MC
+        isData = (not IsMC),
         fixEE2017 = True,
         fixEE2017Params = {'userawPt': True, 'ptThreshold':50.0, 'minEtaThreshold':2.65, 'maxEtaThreshold': 3.139} ,
         postfix = "ModifiedMET"
