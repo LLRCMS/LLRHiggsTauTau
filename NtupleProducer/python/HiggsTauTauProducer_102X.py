0364c44f30005a6cbcf3caed2f9997320423ac5d
Francesco Brivio <francesco.brivio@cern.ch>
2019-12-22 15:20:55 +0100

    Update Global Tags

--- NtupleProducer/python/HiggsTauTauProducer_102X.py
+++ NtupleProducer/python/HiggsTauTauProducer_102X.py
@@ -76,7 +76,7 @@ if IsMC:
   if YEAR == 2017:
     process.GlobalTag.globaltag = '94X_mc2017_realistic_v17'        # 2017 MC
   if YEAR == 2018:
-    process.GlobalTag.globaltag = '102X_upgrade2018_realistic_v19'  # 2018 MC
+    process.GlobalTag.globaltag = '102X_upgrade2018_realistic_v20'  # 2018 MC
 else :
   if YEAR == 2016:
     process.GlobalTag.globaltag = '94X_dataRun2_v10'                # 2016 Data
@@ -84,9 +84,9 @@ else :
     process.GlobalTag.globaltag = '94X_dataRun2_v11'                # 2017 Data
   if YEAR == 2018:
     if PERIOD=="D":
-        process.GlobalTag.globaltag = '102X_dataRun2_Prompt_v14'    # 2018D Data
+        process.GlobalTag.globaltag = '102X_dataRun2_Prompt_v15'    # 2018D Data
     else:
-        process.GlobalTag.globaltag = '102X_dataRun2_v11'           # 2018ABC Data
+        process.GlobalTag.globaltag = '102X_dataRun2_v12'           # 2018ABC Data
 
 print "GT: ",process.GlobalTag.globaltag
 
