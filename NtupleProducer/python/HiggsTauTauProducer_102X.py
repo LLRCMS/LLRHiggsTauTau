fad3e98fdfc3b3ba2fda1a21b3f4be017628ace5
Francesco Brivio <francesco.brivio@cern.ch>
2020-02-29 17:28:07 +0100

    update TES values

--- NtupleProducer/python/HiggsTauTauProducer_102X.py
+++ NtupleProducer/python/HiggsTauTauProducer_102X.py
@@ -337,8 +337,7 @@ process.cleanTaus = cms.EDProducer("PATTauCleaner",
         finalCut = cms.string(' '),
 )
 
-# TES corrections: https://twiki.cern.ch/twiki/bin/viewauth/CMS/TauIDRecommendationForRun2#Tau_energy_scale_for_MVA_tau_id
-# for DeepTau: https://indico.cern.ch/event/864131/contributions/3644021/attachments/1946837/3230164/Izaak_TauPOG_TauES_20191118.pdf
+# TES corrections: https://indico.cern.ch/event/887196/contributions/3743090/attachments/1984772/3306737/TauPOG_TES_20200210.pdf
 
 # EES corrections: https://indico.cern.ch/event/868279/contributions/3665970/attachments/1959265/3267731/FES_9Dec_explained.pdf
 
@@ -346,46 +345,46 @@ process.cleanTaus = cms.EDProducer("PATTauCleaner",
 APPLYTESCORRECTION = APPLYTESCORRECTION if IsMC else False # always false if data
 
 # 2016 data - MVAoldDM2017v2
-#NomTESUncDM0      = cms.double(1.0)  # in percent, up/down uncertainty of TES
+#NomTESUncDM0   = cms.double(1.0)  # in percent, up/down uncertainty of TES
 #NomTESUncDM1   = cms.double(0.9)  # in percent, up/down uncertainty of TES
-#NomTESUncDM10      = cms.double(1.1)  # in percent, up/down uncertainty of TES
-#NomTESUncDM11   = --> Missing <--  # in percent, up/down uncertainty of TES
-#NomTESCorDM0      = cms.double(-0.6) # DecayMode==0
+#NomTESUncDM10  = cms.double(1.1)  # in percent, up/down uncertainty of TES
+#NomTESUncDM11  = --> Missing <--  # in percent, up/down uncertainty of TES
+#NomTESCorDM0   = cms.double(-0.6) # DecayMode==0
 #NomTESCorDM1   = cms.double(-0.5) # DecayMode==1
-#NomTESCorDM10      = cms.double(0.0)  # DecayMode==10
-#NomTESCorDM11   = --> Missing <--  # DecayMode==11
+#NomTESCorDM10  = cms.double(0.0)  # DecayMode==10
+#NomTESCorDM11  = --> Missing <--  # DecayMode==11
 
 # 2017 data - MVAoldDM2017v2
 #if YEAR == 2017:
-#    NomTESUncDM0      = cms.double(0.8)  # in percent, up/down uncertainty of TES
+#    NomTESUncDM0   = cms.double(0.8)  # in percent, up/down uncertainty of TES
 #    NomTESUncDM1   = cms.double(0.8)  # in percent, up/down uncertainty of TES
-#    NomTESUncDM10      = cms.double(0.9)  # in percent, up/down uncertainty of TES
-#    NomTESUncDM11   = cms.double(1.0)  # in percent, up/down uncertainty of TES
-#    NomTESCorDM0      = cms.double(0.7)  # DecayMode==0
+#    NomTESUncDM10  = cms.double(0.9)  # in percent, up/down uncertainty of TES
+#    NomTESUncDM11  = cms.double(1.0)  # in percent, up/down uncertainty of TES
+#    NomTESCorDM0   = cms.double(0.7)  # DecayMode==0
 #    NomTESCorDM1   = cms.double(-0.2) # DecayMode==1
-#    NomTESCorDM10      = cms.double(0.1)  # DecayMode==10
-#    NomTESCorDM11   = cms.double(-0.1) # DecayMode==11
+#    NomTESCorDM10  = cms.double(0.1)  # DecayMode==10
+#    NomTESCorDM11  = cms.double(-0.1) # DecayMode==11
 
 # 2018 data - MVAoldDM2017v2
 #if YEAR == 2018:
-#    NomTESUncDM0      = cms.double(1.1)  # in percent, up/down uncertainty of TES
+#    NomTESUncDM0   = cms.double(1.1)  # in percent, up/down uncertainty of TES
 #    NomTESUncDM1   = cms.double(0.9)  # in percent, up/down uncertainty of TES
-#    NomTESUncDM10      = cms.double(0.8)  # in percent, up/down uncertainty of TES
-#    NomTESUncDM11   = --> Missing <--  # in percent, up/down uncertainty of TES
-#    NomTESCorDM0      = cms.double(-1.3) # DecayMode==0
+#    NomTESUncDM10  = cms.double(0.8)  # in percent, up/down uncertainty of TES
+#    NomTESUncDM11  = --> Missing <--  # in percent, up/down uncertainty of TES
+#    NomTESCorDM0   = cms.double(-1.3) # DecayMode==0
 #    NomTESCorDM1   = cms.double(-0.5) # DecayMode==1
-#    NomTESCorDM10      = cms.double(-1.2) # DecayMode==10
-#    NomTESCorDM11   = --> Missing <--  # DecayMode==11
+#    NomTESCorDM10  = cms.double(-1.2) # DecayMode==10
+#    NomTESCorDM11  = --> Missing <--  # DecayMode==11
 
 # 2016 data - DeepTau2017v2p1
-NomTESUncDM0      = cms.double(0.7)  # in percent, up/down uncertainty of TES
-NomTESUncDM1      = cms.double(0.3)  # in percent, up/down uncertainty of TES
-NomTESUncDM10     = cms.double(0.4)  # in percent, up/down uncertainty of TES
-NomTESUncDM11     = cms.double(0.6)  # in percent, up/down uncertainty of TES
-NomTESCorDM0      = cms.double(-1.0) # DecayMode==0
+NomTESUncDM0      = cms.double(0.8)  # in percent, up/down uncertainty of TES
+NomTESUncDM1      = cms.double(0.6)  # in percent, up/down uncertainty of TES
+NomTESUncDM10     = cms.double(0.8)  # in percent, up/down uncertainty of TES
+NomTESUncDM11     = cms.double(1.1)  # in percent, up/down uncertainty of TES
+NomTESCorDM0      = cms.double(-0.9) # DecayMode==0
 NomTESCorDM1      = cms.double(-0.1) # DecayMode==1
-NomTESCorDM10     = cms.double(0.0)  # DecayMode==10
-NomTESCorDM11     = cms.double(2.6)  # DecayMode==11
+NomTESCorDM10     = cms.double(0.3)  # DecayMode==10
+NomTESCorDM11     = cms.double(-0.2) # DecayMode==11
 
 #EES BARREL
 NomEFakeESCorDM0B     = cms.double(0.679) #DecayMode==0
@@ -394,7 +393,6 @@ NomEFakeESUncDM0BDown  = cms.double(0.982) #DecayMode==0
 NomEFakeESCorDM1B      = cms.double(3.389) #DecayMode==1
 NomEFakeESUncDM1BUp    = cms.double(1.168) #DecayMode==1
 NomEFakeESUncDM1BDown  = cms.double(2.475) #DecayMode==1
-
 #EES ENDCAP
 NomEFakeESCorDM0E      = cms.double(-3.5)   #DecayMode==0
 NomEFakeESUncDM0EUp    = cms.double(1.808)  #DecayMode==0
@@ -405,14 +403,14 @@ NomEFakeESUncDM1EDown  = cms.double(5.694)  #DecayMode==1
 
 # 2017 data - DeepTau2017v2p1
 if YEAR == 2017:
-    NomTESUncDM0      = cms.double(0.7)  # in percent, up/down uncertainty of TES
-    NomTESUncDM1      = cms.double(0.3)  # in percent, up/down uncertainty of TES
-    NomTESUncDM10     = cms.double(0.5)  # in percent, up/down uncertainty of TES
-    NomTESUncDM11     = cms.double(0.6)  # in percent, up/down uncertainty of TES
-    NomTESCorDM0      = cms.double(-0.7) # DecayMode==0
-    NomTESCorDM1      = cms.double(-1.1) # DecayMode==1
-    NomTESCorDM10     = cms.double(0.5)  # DecayMode==10
-    NomTESCorDM11     = cms.double(1.7)  # DecayMode==1
+    NomTESUncDM0      = cms.double(1.0)  # in percent, up/down uncertainty of TES
+    NomTESUncDM1      = cms.double(0.6)  # in percent, up/down uncertainty of TES
+    NomTESUncDM10     = cms.double(0.7)  # in percent, up/down uncertainty of TES
+    NomTESUncDM11     = cms.double(1.4)  # in percent, up/down uncertainty of TES
+    NomTESCorDM0      = cms.double(0.4)  # DecayMode==0
+    NomTESCorDM1      = cms.double(0.2)  # DecayMode==1
+    NomTESCorDM10     = cms.double(0.1)  # DecayMode==10
+    NomTESCorDM11     = cms.double(-1.3) # DecayMode==1
 
     #EES BARREL
     NomEFakeESCorDM0B      = cms.double(0.911) #DecayMode==0
@@ -431,14 +429,14 @@ if YEAR == 2017:
 
 # 2018 data - DeepTau2017v2p1
 if YEAR == 2018:
-    NomTESUncDM0          = cms.double(0.8)  # in percent, up/down uncertainty of TES
-    NomTESUncDM1          = cms.double(0.3)  # in percent, up/down uncertainty of TES
-    NomTESUncDM10         = cms.double(0.4)  # in percent, up/down uncertainty of TES
-    NomTESUncDM11         = cms.double(1.0)  # in percent, up/down uncertainty of TES
+    NomTESUncDM0          = cms.double(0.9)  # in percent, up/down uncertainty of TES
+    NomTESUncDM1          = cms.double(0.5)  # in percent, up/down uncertainty of TES
+    NomTESUncDM10         = cms.double(0.7)  # in percent, up/down uncertainty of TES
+    NomTESUncDM11         = cms.double(1.2)  # in percent, up/down uncertainty of TES
     NomTESCorDM0          = cms.double(-1.6) # DecayMode==0
-    NomTESCorDM1          = cms.double(0.8)  # DecayMode==1
-    NomTESCorDM10         = cms.double(-0.9) # DecayMode==10
-    NomTESCorDM11         = cms.double(1.3)  # DecayMode==11
+    NomTESCorDM1          = cms.double(-0.5) # DecayMode==1
+    NomTESCorDM10         = cms.double(-1.2) # DecayMode==10
+    NomTESCorDM11         = cms.double(-0.4) # DecayMode==11
 
     #EES BARREL
     NomEFakeESCorDM0B      = cms.double(1.362)    #DecayMode==0
