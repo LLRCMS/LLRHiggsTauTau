1ae4de37115a39cf94418271f566233c34a95fa3
Davide Zuolo <davide.zuolo@cern.ch>
2021-06-16 11:22:37 +0200

    Removing MET Ecal bad calibration filter sequence from new producer - not needed for UL

--- NtupleProducer/python/HiggsTauTauProducer_106X.py
+++ NtupleProducer/python/HiggsTauTauProducer_106X.py
@@ -677,38 +677,6 @@ else:
     process.METSequence += process.ShiftMETforEES
     process.METSequence += process.ShiftMETcentral
 
-
-    # 2017 and 2018 ECAL bad calibration filter to be rerun, fix from:
-    # https://twiki.cern.ch/twiki/bin/viewauth/CMS/MissingETOptionalFiltersRun2#How_to_run_ecal_BadCalibReducedM
-    process.load('RecoMET.METFilters.ecalBadCalibFilter_cfi')
-
-    # In 2017 and 2018 some problematic crystals --> pass list of crystals
-    if YEAR == 2017 or YEAR == 2018:
-        baddetEcallist = cms.vuint32(
-            [872439604,872422825,872420274,872423218,
-            872423215,872416066,872435036,872439336,
-            872420273,872436907,872420147,872439731,
-            872436657,872420397,872439732,872439339,
-            872439603,872422436,872439861,872437051,
-            872437052,872420649,872422436,872421950,
-            872437185,872422564,872421566,872421695,
-            872421955,872421567,872437184,872421951,
-            872421694,872437056,872437057,872437313])
-
-    # In 2016 no problem --> pass empty list
-    if YEAR == 2016:
-        baddetEcallist = cms.vuint32([])
-
-    process.ecalBadCalibReducedMINIAODFilter = cms.EDFilter(
-        "EcalBadCalibFilter",
-        EcalRecHitSource = cms.InputTag("reducedEgamma:reducedEERecHits"),
-        ecalMinEt        = cms.double(50.),
-        baddetEcal    = baddetEcallist,
-        taggingMode = cms.bool(True),
-        debug = cms.bool(False)
-        )
-
-
 ## ----------------------------------------------------------------------
 ## Z-recoil correction
 ## ----------------------------------------------------------------------
